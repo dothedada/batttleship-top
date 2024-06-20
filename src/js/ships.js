e1b8d559d7333446bfe6b1b56910349e12bed7c1
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-20 15:03:19 -0500

    Crea la clase Ship

--- src/js/ships.js
+++ src/js/ships.js
@@ -1,5 +1,18 @@
 class Ship {
-    //
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        this.hits++;
+        this.isSunk();
+    }
+
+    isSunk() {
+        this.sunk = this.length === this.hits;
+    }
 }
 
-export default Ship
+export default Ship;
