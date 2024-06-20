cc632dbf3c222245cb4c1a3521891996ccdbcbf5
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-20 14:40:36 -0500

    crea test para class Ship

--- src/js/tests/ships.test.js
+++ src/js/tests/ships.test.js
@@ -1,7 +1,35 @@
-import ship from "../ships";
+import Ship from '../ships';
 
 describe('Test para ship', () => {
-    test('Ship tiene la cantidad de impactos asugnada', () => {
-        expect(ship(9)).toBe(9)
-    })
-})
+    const carrier = Ship(5);
+    const battleship = Ship(4);
+    const cruiser = Ship(3);
+    const destroyer = Ship(2);
+
+    test('Los barcos son objetos', () => {
+        expect(typeof destroyer === 'object').toBe(true);
+    });
+
+    test('Los barcos tienen la longitud esperada', () => {
+        expect(carrier.length).toBe(5);
+        expect(battleship.length).toBe(4);
+        expect(cruiser.length).toBe(3);
+        expect(destroyer.length).toBe(2);
+    });
+
+    test('Cada hit() reduce 1 hits y el inventario de daños se actualiza', () => {
+        for (let i = 0; i < carrier.length; i--) {
+            expect(carrier.hits).toBe(i);
+            carrier.hit();
+        }
+    });
+
+    test('Con cada impacto evalua si el barco se hunde', () => {
+        for (let i = cruiser.length; i > 1; i--) {
+            carrier.hit();
+            expect(cruiser.sunk).toBe(false);
+        }
+        carrier.hit();
+        expect(cruiser.sunk).toBe(true);
+    });
+});
