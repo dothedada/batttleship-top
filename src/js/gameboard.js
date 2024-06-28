9017ca4d113eae815d027a7fc7dfbc4e822fec79
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-27 21:15:53 -0500

    Crea los metodos base para ubicar los barcos

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,8 +1,53 @@
-import Ship from "./ships"
+import Ship, { typeOfShips } from './ships';
 
 class Gameboard {
-    
-    //
+    constructor() {
+        this.ships = this.#boardGenerator();
+        this.attacks = this.#boardGenerator();
+    }
+
+    #boardGenerator() {
+        const board = [];
+
+        for (let i = 0; i < 10; i++) {
+            board.push([]);
+            for (let j = 0; j < 10; j++) {
+                board[i].push(false);
+            }
+        }
+
+        return board;
+    }
+
+    placeShip(col, row, vertical, type) {
+        
+        const ship = new Ship(type)
+        this.#shipsAvailable.add(type)
+        // const cols = vertical && col + ship.length >= 10 ? 10 - ship.length : col
+        // const rows = vertical && row + ship.length >= 10 ? 10 - ship.length : row
+        for (let l = 0; l < ship.length; l++) {
+            const i = vertical ? row + l : row
+            const j = !vertical ? col + l : col
+
+            if (this.ships[i][j]) return false
+        }
+
+        for (let l = 0; l < ship.length; l++) {
+            const i = vertical ? row + l : row
+            const j = !vertical ? col + l : col
+
+            this.ships[i][j] = ship
+        }
+        
+        return true
+    }
+
+    #shipsAvailable = new Set()
+
+    shipsLeft() {
+        return 5 - this.#shipsAvailable.size
+    }
+
 }
 
-export default Gameboard
+export default Gameboard;
