76627d5e370d849434d812f58b5c33a423d761cf
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-27 21:15:14 -0500

    Actualiza los test

--- src/js/tests/gameboard.test.js
+++ src/js/tests/gameboard.test.js
@@ -2,53 +2,43 @@ import Gameboard from '../gameboard';
 const board = new Gameboard();
 
 const shipCells = (arr = [], ship = undefined) => {
-    return arr.reduce((sum, current) => {
-        if (Array.isArray(current)) {
-            sum = shipCells(current);
-            return sum;
-        }
-
-        if (ship && ship === current) {
-            sum++;
-            return sum;
-        }
-
-        if (current) sum++;
-        return sum;
-    });
+    const array = arr.flat();
+    if (ship) {
+        return array.filter((e) => e.type === ship).length;
+    } else {
+        return array.filter((e) => !e).length;
+    }
 };
 
 describe('Evaluación del tablero', () => {
     test('El tablero es un array 2D de 10x10', () => {
-        expect(Array.isArray(board.table)).toBe(true);
-        expect(Array.isArray(board.table.length)).toBe(10);
-        expect(Array.isArray(board.table[9])).toBe(true);
-        expect(Array.isArray(board.table[9].length)).toBe(10);
+        expect(Array.isArray(board.ships)).toBe(true);
+        expect(board.ships.length).toBe(10);
+        expect(Array.isArray(board.ships[9])).toBe(true);
+        expect(board.ships[9].length).toBe(10);
     });
 
     test('Todos los campos están inicializados con false', () => {
-        expect(
-            board.table.every((box) => box[0] === false && box[1] === false),
-        ).toBe(true);
+        expect(shipCells(board.ships, true)).toBe(0);
     });
 });
 
 describe('Administración y posicionamiento de barcos', () => {
-    test('Ubica un barco verticalmente dentro del tablero', () => {
-        expect(board.palceShip(0, 2, false, 'Carrier')).toBe(true);
+    test('Ubica un barco horizontalmente dentro del tablero', () => {
+        expect(board.placeShip(0, 2, false, 'Carrier')).toBe(true);
         for (let i = 0; i < 5; i++) {
-            expect(board.table[i][2]).toBe('K');
+            expect(board.ships[2][i].type).toBe('Carrier');
         }
     });
 
     test('Sólo son marcadas las celdas ocupadas por el barco', () => {
-        expect(shipCells(board.table, 'K')).toBe(5);
+        expect(shipCells(board.ships, 'Carrier')).toBe(5);
     });
 
-    test('Ubica un barco horizontalmente dentro del tablero', () => {
-        expect(board.palceShip(7, 0, true, 'Battleship')).toBe(true);
+    test('Ubica un barco verticalmente dentro del tablero', () => {
+        expect(board.placeShip(7, 0, true, 'Battleship')).toBe(true);
         for (let i = 0; i < 4; i++) {
-            expect(board.table[7][i]).toBe('B');
+            expect(board.ships[i][7].type).toBe('Battleship');
         }
     });
 
@@ -57,72 +47,76 @@ describe('Administración y posicionamiento de barcos', () => {
     });
 
     test('No permite la ubicación de un barco donde por donde ya pasa otro, mismo sentido', () => {
-        expect(board.palceShip(4, 2, false, 'Cruiser')).toThrow(
-            new Error('No se puede ubicar un barco donde ya existe otro'),
-        );
+        expect(board.placeShip(4, 2, false, 'Cruiser')).toBe(false);
     });
 
     test('No permite la ubicación de un barco donde por donde ya pasa otro, sentido contrario', () => {
-        expect(board.palceShip(2, 1, true, 'Cruiser')).toThrow(
-            new Error('No se puede ubicar un barco donde ya existe otro'),
-        );
+        expect(board.placeShip(2, 1, true, 'Cruiser')).toBe(false);
     });
 
     test('La ubicación de un barco horizontal nunca excede el límite', () => {
-        expect(board.palceShip(9, 6, false, 'Cruiser')).toBe(true);
+        expect(board.placeShip(9, 6, false, 'Cruiser')).toBe(true);
         for (let i = 7; i < 10; i++) {
-            expect(board.table[i][6]).toBe('C');
+            expect(board.ships[i][6].type).toBe('Cruiser');
         }
     });
 
     test('La ubicación de un barco vertical nunca excede el límite', () => {
-        expect(board.palceShip(9, 6, true, 'Submarine')).toBe(true);
+        expect(board.placeShip(6, 6, true, 'Submarine')).toBe(true);
+        console.table(board.ships);
         for (let i = 7; i < 10; i++) {
-            expect(board.table[4][i]).toBe('S');
+            expect(board.ships[4][i].type).toBe('Submarine');
         }
     });
-
-    test('Mantiene el inventario de los barcos ubicados 2', () => {
-        expect(board.shipsLeft()).toBe(1);
-    });
-
-    test('ubica aleatoriamente un barco', () => {
-        expect(placeShipRandom('Destroyer')).toBe(true);
-        expect(shipCells(board.table, 'D')).toBe(2);
-    });
-
-    test('Las celdas del barco ubicado aleatoriamente son continuas', () => {
-        const flatBoard = board.table.flat();
-        const differenceOfIndexes =
-            flatBoard.lastIndexOf('D') - flatBoard.findIndex('D');
-
-        expect([1,10]).toContain(differenceOfIndexes);
-    });
-
-    test('Sólo son marcadas las celdas ocupadas por kos barcos 2', () => {
-        expect(shipCells(board.table)).toBe(17);
-    });
+    //
+    // test('Mantiene el inventario de los barcos ubicados 2', () => {
+    //     expect(board.shipsLeft()).toBe(1);
+    // });
+    //
+    // test('ubica aleatoriamente un barco', () => {
+    //     expect(placeShipRandom('Destroyer')).toBe(true);
+    //     expect(shipCells(board.ships, 'D')).toBe(2);
+    // });
+    //
+    // test('Las celdas del barco ubicado aleatoriamente son continuas', () => {
+    //     const flatBoard = board.ships.flat();
+    //     const differenceOfIndexes =
+    //         flatBoard.lastIndexOf('D') - flatBoard.findIndex('D');
+    //
+    //     expect([1, 10]).toContain(differenceOfIndexes);
+    // });
+    //
+    // test('Sólo son marcadas las celdas ocupadas por kos barcos 2', () => {
+    //     expect(shipCells(board.ships)).toBe(17);
+    // });
 });
 
 describe('Elementos del juego', () => {
-    test('Marca en el disparo en una celda desocupada', () => {
-        //
-    });
-
-    test('Marca el disparo en una celda ocupada', () => {
-        //
-    });
-
-    test('Cuando el disparo da en un bote, se actualiza el estado de este', () => {
-        //
-    });
-
-    test('Reporta el hundimiento de un bote', () => {
-        //
-    });
-
-    test('avisa cuando ya no le quedan barcos en este tablero', () => {
-        //
-    });
+    // test('Marca en el disparo en una celda desocupada', () => {
+    //     expect(board.receiveAttack(0, 0)).toBe(true);
+    //     expect(board.ships[0][0]).toBe('');
+    // });
     //
+    // test('No permite repetir ubicación de disparo', () => {
+    //     expect(board.receiveAttack(0, 0)).toBe(false);
+    // });
+    //
+    // test('Marca el disparo en una celda ocupada', () => {
+    //     expect(board.receiveAttack(0, 0)).toBe(true);
+    //     expect(board.ships[0][0]).toBe('X');
+    //     //
+    // });
+    //
+    // test('Cuando el disparo da en un bote, se actualiza el estado de este', () => {
+    //     //
+    // });
+    //
+    // test('Reporta el hundimiento de un bote', () => {
+    //     //
+    // });
+    //
+    // test('avisa cuando ya no le quedan barcos en este tablero', () => {
+    //     //
+    // });
+    // //
 });
