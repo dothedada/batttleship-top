9460808cc858d426f8d89ee6b62eee8474de37cf
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-30 19:58:39 -0500

    inicia refactorizada de player, ajusta vinculación de jugadores

--- src/js/players.js
+++ src/js/players.js
@@ -1,136 +1,147 @@
 import Gameboard from './gameboard';
-import { typeOfShips } from './ships';
 
 class Player {
-    constructor(type) {
+    constructor(name = undefined) {
         this.board = new Gameboard();
-        this.human = type === 'Human';
-        if (!this.human) {
-            this.nextAttack = { hits: [], queue: undefined };
-        }
-    }
+        this.myShips = this.board.ships;
+        this.myAttacks = Gameboard.boardGenerator();
+        this.adversaryName = undefined;
 
-    placeAllShips() {
-        for (const ship of Object.keys(typeOfShips)) {
-            this.board.placeShipRandom(ship);
+        if (name) {
+            this.name = name;
+        } else {
+            this.nextAttack = { hits: [], queue: undefined };
         }
     }
 
     #adversary = undefined;
 
     setAdversary(player) {
-        if (!(player instanceof Player) || this.#adversary) {
-            return false;
+        if (!(player instanceof Player) || this.adversaryName) {
+            return;
         }
+
         this.#adversary = player;
-        return true;
-    }
+        this.adversaryName = this.#adversary.name ?? 'AutoPlayer';
 
-    #hitOrMiss(col, row) {
-        if (typeof this.#adversary.shipsBoard[row][col] === 'object') {
-            this.#adversary.board.receiveAttack(col, row);
-            return true;
-        }
-        return false;
-    }
-
-    attack(col, row) {
-        if (col > 9 || row > 9 || this.board.attacks[row][col]) {
-            return false;
-        }
-
-        const itsAHit = this.#hitOrMiss(col, row);
-        this.board.attacks[row][col] = itsAHit ? 'X' : '·';
-
-        if (!this.human && itsAHit) {
-            const neighbors = [-10, 1, 10, -1];
-
-        const lastHit = this.nextAttack.hits[this.nextAttack.hits.length - 1];
-
-            this.nextAttack.hits.push(`${row}${col}`.padStart(2, '0'));
-            this.nextAttack.queue =
-                this.nextAttack.queue ??
-                neighbors.reduce((availabe, position) => {
-                    const [nRow, nCol] = `${+lastHit + position}`.padStart(
-                        2,
-                        '0',
-                    );
-
-                    if (!this.attacksBoard[nRow][nCol]) {
-                        availabe.push(`${nRow}${nCol}`.padStart(2, '0'));
-                    }
-
-                    return availabe;
-                }, []);
-        }
-
-        return true;
-    }
-
-    autoAtack() {
-        if (!this.human && this.nextAttack.hits.length) {
-            this.queueAttack();
-        } else {
-            this.randomAttack();
+        if (!player.adversaryName) {
+            player.setAdversary(this);
         }
     }
 
-    randomAttack() {
-        const availableShoots = this.attacksBoard
-            .flat()
-            .reduce((sum, cell, index) => {
-                if (!cell) {
-                    sum.push(`${index}`.padStart(2, 0));
-                }
-
-                return sum;
-            }, []);
-
-        const [randomRow, randomCol] =
-            availableShoots[
-                Math.floor(Math.random() * availableShoots.length)
-            ].split('');
-
-        this.attack(+randomCol, +randomRow);
-    }
-
-    queueAttack() {
-        const lastHit = this.nextAttack.hits[this.nextAttack.hits.length - 1];
-        const hitSecuence = this.nextAttack.hits;
-
-        if (!this.nextAttack.queue && this.nextAttack.hits.length === 1) {
-        } else {
-            hitSecuence.sort((a, b) => a - b);
-
-            const [first, second, previous, last] = [
-                +hitSecuence[0],
-                +hitSecuence[1],
-                +hitSecuence[hitSecuence.length - 2],
-                +hitSecuence[hitSecuence.length - 1],
-            ];
-
-            const follow = `${last + (last - previous)}`.padStart(2, '0');
-            const fallback = `${first - (second - first)}`.padStart(2, '0');
-
-            this.nextAttack.queue = [];
-
-            if (this.attacksBoard[follow[0]][follow[1]] === false) {
-                this.nextAttack.queue.push(follow);
-            }
-            if (this.attacksBoard[fallback[0]][fallback[1]] === false) {
-                this.nextAttack.queue.push(fallback);
-            }
-        }
-        //
-    }
-
-    get shipsBoard() {
-        return this.board.ships;
-    }
-
-    get attacksBoard() {
-        return this.board.attacks;
-    }
+    // placeAllShips() {
+    //     for (const ship of Object.keys(typeOfShips)) {
+    //         this.board.placeShipRandom(ship);
+    //     }
+    // }
+    //
+    //
+    //
+    // #hitOrMiss(col, row) {
+    //     if (typeof this.#adversary.shipsBoard[row][col] === 'object') {
+    //         this.#adversary.board.receiveAttack(col, row);
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    //
+    // attack(col, row) {
+    //     if (col > 9 || row > 9 || this.board.attacks[row][col]) {
+    //         return false;
+    //     }
+    //
+    //     const itsAHit = this.#hitOrMiss(col, row);
+    //     this.board.attacks[row][col] = itsAHit ? 'X' : '·';
+    //
+    //     if (!this.human && itsAHit) {
+    //         const neighbors = [-10, 1, 10, -1];
+    //
+    //     const lastHit = this.nextAttack.hits[this.nextAttack.hits.length - 1];
+    //
+    //         this.nextAttack.hits.push(`${row}${col}`.padStart(2, '0'));
+    //         this.nextAttack.queue =
+    //             this.nextAttack.queue ??
+    //             neighbors.reduce((availabe, position) => {
+    //                 const [nRow, nCol] = `${+lastHit + position}`.padStart(
+    //                     2,
+    //                     '0',
+    //                 );
+    //
+    //                 if (!this.attacksBoard[nRow][nCol]) {
+    //                     availabe.push(`${nRow}${nCol}`.padStart(2, '0'));
+    //                 }
+    //
+    //                 return availabe;
+    //             }, []);
+    //     }
+    //
+    //     return true;
+    // }
+    //
+    // autoAtack() {
+    //     if (!this.human && this.nextAttack.hits.length) {
+    //         this.queueAttack();
+    //     } else {
+    //         this.randomAttack();
+    //     }
+    // }
+    //
+    // randomAttack() {
+    //     const availableShoots = this.attacksBoard
+    //         .flat()
+    //         .reduce((sum, cell, index) => {
+    //             if (!cell) {
+    //                 sum.push(`${index}`.padStart(2, 0));
+    //             }
+    //
+    //             return sum;
+    //         }, []);
+    //
+    //     const [randomRow, randomCol] =
+    //         availableShoots[
+    //             Math.floor(Math.random() * availableShoots.length)
+    //         ].split('');
+    //
+    //     this.attack(+randomCol, +randomRow);
+    // }
+    //
+    // queueAttack() {
+    //     const lastHit = this.nextAttack.hits[this.nextAttack.hits.length - 1];
+    //     const hitSecuence = this.nextAttack.hits;
+    //
+    //     if (!this.nextAttack.queue && this.nextAttack.hits.length === 1) {
+    //     } else {
+    //         hitSecuence.sort((a, b) => a - b);
+    //
+    //         const [first, second, previous, last] = [
+    //             +hitSecuence[0],
+    //             +hitSecuence[1],
+    //             +hitSecuence[hitSecuence.length - 2],
+    //             +hitSecuence[hitSecuence.length - 1],
+    //         ];
+    //
+    //         const follow = `${last + (last - previous)}`.padStart(2, '0');
+    //         const fallback = `${first - (second - first)}`.padStart(2, '0');
+    //
+    //         this.nextAttack.queue = [];
+    //
+    //         if (this.attacksBoard[follow[0]][follow[1]] === false) {
+    //             this.nextAttack.queue.push(follow);
+    //         }
+    //         if (this.attacksBoard[fallback[0]][fallback[1]] === false) {
+    //             this.nextAttack.queue.push(fallback);
+    //         }
+    //     }
+    //     //
+    // }
+    //
+    // get shipsBoard() {
+    //     return this.board.ships;
+    // }
+    //
+    // get attacksBoard() {
+    //     return this.board.attacks;
+    // }
 }
 
 export default Player;
--- src/js/tests/players.test.js
+++ src/js/tests/players.test.js
@@ -1,131 +1,146 @@
+import Gameboard from '../gameboard';
 import Player from '../players';
 
-const player1 = new Player('Human');
-const player2 = new Player('Human');
-const playerC = new Player('Computer');
-const playerC2 = new Player('Computer');
+const player1 = new Player('Miguel');
+const player2 = new Player('Andrea');
+const playerC = new Player();
+const playerC2 = new Player('Arturito');
 
 describe('Métodos para el Jugador humano', () => {
-    test('Cada jugador tiene su tablero', () => {
-        expect(Array.isArray(player1.board.ships)).toBe(true);
-        expect(Array.isArray(player1.board.attacks)).toBe(true);
+    test('Cada jugador tiene su tablero y los jugadores humanos un nombre', () => {
+        expect(Array.isArray(player1.myShips)).toBe(true);
+        expect(Array.isArray(player1.myAttacks)).toBe(true);
+        expect(player1.board instanceof Gameboard).toBe(true);
+        expect(player1).toHaveProperty('name')
+        expect(playerC).not.toHaveProperty('name')
     });
 
-    test('Asigna un adversario', () => {
-        expect(player1.setAdversary(player2)).toBe(true);
-        expect(player2.setAdversary(player1)).toBe(true);
-        expect(playerC.setAdversary(playerC2)).toBe(true);
-        expect(playerC2.setAdversary(playerC)).toBe(true);
+    test('Crea queue para jugadas automatizadas del jugador automático', () => {
+        expect(playerC).toHaveProperty('nextAttack');
+        expect(playerC).toHaveProperty('nextAttack.hits');
+        expect(playerC).toHaveProperty('nextAttack.queue');
     });
 
-    test('Cada jugador dispone de 5 barcos al iniciar el tablero', () => {
-        expect(player1.board.shipsLeftToPlace()).toBe(5);
+    test('Vincula los adversarios', () => {
+        player1.setAdversary(player2)
+        playerC.setAdversary(playerC2)
+
+        expect(player1.adversaryName).toBe('Andrea')
+        expect(player2.adversaryName).toBe('Miguel')
+        expect(playerC.adversaryName).toBe('Arturito')
+        expect(playerC2.adversaryName).toBe('AutoPlayer')
     });
 
-    test('El contador se actualiza cada que ubica un barco', () => {
-        expect(player1.board.placeShip(0, 2, false, 'Carrier')).toBe(true);
-        expect(player1.board.shipsLeftToPlace()).toBe(4);
-        expect(player1.board.placeShip(7, 0, true, 'Battleship')).toBe(true);
-        expect(player1.board.shipsLeftToPlace()).toBe(3);
-        expect(player1.board.placeShip(7, 0, false, 'Cruiser')).toBe(false);
-        expect(player1.board.shipsLeftToPlace()).toBe(3);
-        expect(player1.board.placeShip(7, 6, false, 'Cruiser')).toBe(true);
-        expect(player1.board.shipsLeftToPlace()).toBe(2);
-        expect(player1.board.placeShip(4, 9, true, 'Submarine')).toBe(true);
-        expect(player1.board.shipsLeftToPlace()).toBe(1);
-        expect(player1.board.placeShip(1, 5, false, 'Destroyer')).toBe(true);
-        expect(player1.board.shipsLeftToPlace()).toBe(0);
-    });
-
-    test('No permite ningun disparo fuera del tablero', () => {
-        expect(player1.attack(0, 0)).toBe(true);
-        expect(player1.attack(10, 10)).toBe(false);
-        expect(player1.board.attacks[0][0]).toBe('·');
-    });
-
-    test('No permite disparos en donde ya han disparado', () => {
-        expect(player1.attack(0, 0)).toBe(false);
-    });
-
-    test('Muestra la ubicación de los barcos y disparos recibidos', () => {
-        expect(Array.isArray(player1.shipsBoard)).toBe(true);
-    });
-
-    test('Muestra la ubicación de los disparos realizados', () => {
-        expect(Array.isArray(player1.attacksBoard)).toBe(true);
-    });
-
-    test('Marca los disparos que dieron en algun barco del enemigo', () => {
-        player2.board.placeShip(3, 2, true, 'Carrier');
-        expect(player1.attack(3, 2)).toBe(true);
-        expect(player1.board.attacks[2][3]).toBe('X');
-        expect(player2.board.ships[2][3]).toBe('X');
-    });
-});
-
-describe('Setup base para el Jugador automático', () => {
-    test('Crea attackQueue para jugador automatico', () => {
-        expect(typeof playerC.nextAttack === 'object').toBe(true);
-        expect(playerC.nextAttack.hits).toBeTruthy();
-        expect(playerC.nextAttack.queue).toBeUndefined();
-        expect(player1.nextAttack).toBeUndefined();
-    });
-    test('ubica sus 5 barcos de forma aleatoria', () => {
-        playerC.placeAllShips();
-        expect(playerC.shipsBoard.flat().filter((e) => e.type).length).toBe(17);
-    });
-
-    test('Luego de 100 disparos aleatorios ha cubierto todo el tablero', () => {
-        for (let i = 0; i < 100; i++) {
-            playerC.autoAtack();
-        }
-        expect(
-            playerC.attacksBoard.flat().filter((e) => e === false).length,
-        ).toBe(0);
-    });
-});
-
-describe('Comportamiento de los disparos de la computadora desde el impacto hasta hundir un bote', () => {
-    test('Con el primer impacto marca el punto y la secuencia de ataque a seguir', () => {
-        playerC.board.resetAttacksBoard();
-        playerC2.board.placeShip(1, 1, true, 'Carrier');
-        playerC.attack(1, 1);
-
-        expect(playerC.nextAttack.hits.length).toBe(1)
-        expect(playerC.nextAttack.hits[0]).toBe('11')
-        
-        console.log(playerC.nextAttack.queue.length)
-        // expect(playerC.nextAttack.queue.length).toBe(4)
-        // expect(playerC.nextAttack.queue[0]).toBe('01')
-        // expect(playerC.nextAttack.queue[1]).toBe('12')
-        // expect(playerC.nextAttack.queue[2]).toBe('21')
-        // expect(playerC.nextAttack.queue[3]).toBe('10')
-        
-    });
-
-    // test('Sigue la secuencia de disparos hasta volver a impactar', () => {
-    //     playerC.autoAtack()
-    //     expect(playerC.attacksBoard[0][1]).toBe('·')
-    //     playerC.autoAtack()
-    //     expect(playerC.attacksBoard[1][2]).toBe('·')
-    //     expect(playerC.attacksBoard.flat().filter(c => c ==='·').length).toBe(2)
+    // test('la información ')
     //
-    //     playerC.autoAtack()
-    //     playerC.autoAtack()
-    //     playerC.autoAtack()
-    //     playerC.autoAtack()
-    //     playerC.autoAtack()
-    //     console.table(playerC.attacksBoard)
+    // test('Cada jugador dispone de 5 barcos al iniciar el tablero', () => {
+    //     expect(player1.board.shipsLeftToPlace()).toBe(5);
     // });
     //
-    // test('Luego del segundo disparo acertado, prioriza la dirección', () => {
-    //     //
+    // test('El contador se actualiza cada que ubica un barco', () => {
+    //     expect(player1.board.placeShip(0, 2, false, 'Carrier')).toBe(true);
+    //     expect(player1.board.shipsLeftToPlace()).toBe(4);
+    //     expect(player1.board.placeShip(7, 0, true, 'Battleship')).toBe(true);
+    //     expect(player1.board.shipsLeftToPlace()).toBe(3);
+    //     expect(player1.board.placeShip(7, 0, false, 'Cruiser')).toBe(false);
+    //     expect(player1.board.shipsLeftToPlace()).toBe(3);
+    //     expect(player1.board.placeShip(7, 6, false, 'Cruiser')).toBe(true);
+    //     expect(player1.board.shipsLeftToPlace()).toBe(2);
+    //     expect(player1.board.placeShip(4, 9, true, 'Submarine')).toBe(true);
+    //     expect(player1.board.shipsLeftToPlace()).toBe(1);
+    //     expect(player1.board.placeShip(1, 5, false, 'Destroyer')).toBe(true);
+    //     expect(player1.board.shipsLeftToPlace()).toBe(0);
     // });
     //
-    // test('si al tener la dirección y llega al final, dispara en el otro sentido', () => {
-    //     //
+    // test('No permite ningun disparo fuera del tablero', () => {
+    //     expect(player1.attack(0, 0)).toBe(true);
+    //     expect(player1.attack(10, 10)).toBe(false);
+    //     expect(player1.board.attacks[0][0]).toBe('·');
+    // });
+    //
+    // test('No permite disparos en donde ya han disparado', () => {
+    //     expect(player1.attack(0, 0)).toBe(false);
+    // });
+    //
+    // test('Muestra la ubicación de los barcos y disparos recibidos', () => {
+    //     expect(Array.isArray(player1.shipsBoard)).toBe(true);
+    // });
+    //
+    // test('Muestra la ubicación de los disparos realizados', () => {
+    //     expect(Array.isArray(player1.attacksBoard)).toBe(true);
+    // });
+    //
+    // test('Marca los disparos que dieron en algun barco del enemigo', () => {
+    //     player2.board.placeShip(3, 2, true, 'Carrier');
+    //     expect(player1.attack(3, 2)).toBe(true);
+    //     expect(player1.board.attacks[2][3]).toBe('X');
+    //     expect(player2.board.ships[2][3]).toBe('X');
     // });
 });
+
+// describe('Setup base para el Jugador automático', () => {
+//     test('Crea attackQueue para jugador automatico', () => {
+//         expect(typeof playerC.nextAttack === 'object').toBe(true);
+//         expect(playerC.nextAttack.hits).toBeTruthy();
+//         expect(playerC.nextAttack.queue).toBeUndefined();
+//         expect(player1.nextAttack).toBeUndefined();
+//     });
+//     test('ubica sus 5 barcos de forma aleatoria', () => {
+//         playerC.placeAllShips();
+//         expect(playerC.shipsBoard.flat().filter((e) => e.type).length).toBe(17);
+//     });
+//
+//     test('Luego de 100 disparos aleatorios ha cubierto todo el tablero', () => {
+//         for (let i = 0; i < 100; i++) {
+//             playerC.autoAtack();
+//         }
+//         expect(
+//             playerC.attacksBoard.flat().filter((e) => e === false).length,
+//         ).toBe(0);
+//     });
+// });
+
+// describe('Comportamiento de los disparos de la computadora desde el impacto hasta hundir un bote', () => {
+//     test('Con el primer impacto marca el punto y la secuencia de ataque a seguir', () => {
+//         playerC.board.resetAttacksBoard();
+//         playerC2.board.placeShip(1, 1, true, 'Carrier');
+//         playerC.attack(1, 1);
+//
+//         expect(playerC.nextAttack.hits.length).toBe(1)
+//         expect(playerC.nextAttack.hits[0]).toBe('11')
+//
+//         console.log(playerC.nextAttack.queue.length)
+// expect(playerC.nextAttack.queue.length).toBe(4)
+// expect(playerC.nextAttack.queue[0]).toBe('01')
+// expect(playerC.nextAttack.queue[1]).toBe('12')
+// expect(playerC.nextAttack.queue[2]).toBe('21')
+// expect(playerC.nextAttack.queue[3]).toBe('10')
+
+// });
+
+// test('Sigue la secuencia de disparos hasta volver a impactar', () => {
+//     playerC.autoAtack()
+//     expect(playerC.attacksBoard[0][1]).toBe('·')
+//     playerC.autoAtack()
+//     expect(playerC.attacksBoard[1][2]).toBe('·')
+//     expect(playerC.attacksBoard.flat().filter(c => c ==='·').length).toBe(2)
+//
+//     playerC.autoAtack()
+//     playerC.autoAtack()
+//     playerC.autoAtack()
+//     playerC.autoAtack()
+//     playerC.autoAtack()
+//     console.table(playerC.attacksBoard)
+// });
+//
+// test('Luego del segundo disparo acertado, prioriza la dirección', () => {
+//     //
+// });
+//
+// test('si al tener la dirección y llega al final, dispara en el otro sentido', () => {
+//     //
+// });
+// });
 //
 // test('Cuando un jugador queda sin barcos declara el ganador', () => {
 //     //
