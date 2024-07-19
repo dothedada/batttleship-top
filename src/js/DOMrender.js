cafb286a3d8862e306d3af1863e17fc99efa0467
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-07-19 10:54:41 -0500

    Crea la función que renderiza el mapa de barcos

--- src/js/DOMrender.js
+++ src/js/DOMrender.js
@@ -48,36 +48,58 @@ const renderInDOM = (() => {
 
         let row = 1;
         player.myAttacks.flat().forEach((cell, index) => {
+            let dataImpact = '';
+            let disabled = false;
+            let buttonText = '';
 
             if (index % 10 === 0) {
                 board.append(wrapper('span', 'board__coordenates', row));
                 row++;
             }
 
-            let dataImpact = '';
-            let disabled = false;
-            let buttonText = '';
-
-            if (cell === 'X') {
-                dataImpact = 'ship';
+            if (cell) {
+                dataImpact = cell === 'X' ? 'ship' : 'water';
                 disabled = true;
-                buttonText = ' X ';
-            }
-            if (cell === '~') {
-                dataImpact = 'water';
-                disabled = true;
-                buttonText = ' ~ ';
+                buttonText = cell;
             }
 
-            board.append(button(buttonText, 'board__attack', dataImpact, disabled));
+            board.append(
+                button(buttonText, 'board__attack', dataImpact, disabled),
+            );
         });
 
-        return board
-
-
+        return board;
     };
 
-    return { inputText, button, wrapper, attackBoard };
+    const shipsBoard = (player) => {
+        const board = wrapper('div', 'board');
+
+        for (let i = 0; i < 11; i++) {
+            const text = i === 0 ? '\\' : String.fromCharCode(64 + i);
+            const boardHeader = wrapper('span', 'board__coordenates', text);
+            board.append(boardHeader);
+        }
+
+        let row = 1;
+        player.myShips.flat().forEach((cell, index) => {
+            let text = '';
+
+            if (index % 10 === 0) {
+                board.append(wrapper('span', 'board__coordenates', row));
+                row++;
+            }
+
+            if (cell) {
+                text = typeof cell === 'object' ? cell.type.slice(0, 2) : cell;
+            }
+
+            board.append(wrapper('span', 'board__ships', text));
+        });
+
+        return board;
+    };
+
+    return { inputText, button, wrapper, attackBoard, shipsBoard };
 })();
 
 export default renderInDOM;
