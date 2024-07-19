aff6e464945d6cfe17bca15712f90e8cd4e17ff7
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-07-18 21:48:13 -0500

    Hace llamado a la pantalla de creacion de los jugadores

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,63 @@
 import '../css/reset.css';
 import '../css/styles.css';
 import landingScreen from './landing';
+import Player from './players';
+import setPlayers from './setPlayers';
 
 const screen = (() => {
     const current = ['landing', 'setPlayers', 'encounter', 'aftermath'];
     let index = 0;
 
-    const set = () => {
+    const next = () => {
         index++;
 
-        if (index > 3) {
+        if (index > 1) {
             index = 0;
         }
-
     };
 
     const get = () => current[index];
 
-    return { set, get };
+    const clear = () => {
+        document.querySelector('#header').textContent = '';
+        document.querySelector('#app').textContent = '';
+        document.body.removeEventListener('keydown', clear);
+        document.body.removeEventListener('pointerdown', clear);
+
+        renderPage(get());
+    };
+
+    return { next, get, clear };
 })();
 
 const renderPage = (page) => {
-    if (page === 'landing') { landingScreen() };
-}
+    let player1;
+    let player2;
+
+    if (page === 'landing') {
+        landingScreen();
+
+        document.body.addEventListener('pointerdown', screen.clear);
+        document.body.addEventListener('keydown', screen.clear);
+
+        screen.next();
+
+        console.log(player1);
+    }
+
+    if (page === 'setPlayers') {
+        setPlayers();
+
+        document.querySelector('button').addEventListener('pointerdown', () => {
+            const [player1Name, player2Name] =
+                document.querySelectorAll('input');
+            player1 = new Player(player1Name);
+            player2 = new Player(player2Name);
+            player1.setAdversary(player2);
+            screen.clear();
+        });
+
+        screen.next();
+    }
+};
 renderPage(screen.get());
