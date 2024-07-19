c65d3470891e7bebf9b8248e82aa44e7df362239
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-07-18 19:34:06 -0500

    Crea la función de estado del juego

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,29 @@
 import '../css/reset.css';
 import '../css/styles.css';
 
-const testeoDeMinifyEInyeccion = () => {
-    console.log('carajo');
-};
-testeoDeMinifyEInyeccion();
+const screen = (() => {
+    const current = ['landing', 'setPlayers', 'encounter', 'aftermath'];
+    let index = 0;
+
+    const set = () => {
+        index++;
+
+        if (index > 3) {
+            index = 0;
+        }
+    };
+
+    const get = () => current[index];
+
+    return { set, get };
+})();
+
+console.log(screen.get());
+screen.set();
+console.log(screen.get());
+screen.set();
+console.log(screen.get());
+screen.set();
+console.log(screen.get());
+screen.set();
+console.log(screen.get());
