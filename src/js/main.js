08088b8079f8b864d3bf0058bd3d184a6fc14194
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-07-24 10:21:36 -0500

    Crea la pantalla de inicio

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import '../css/styles.css';
 import asciiArt from './asciiArt';
 import {
+    clearApp,
     wrapper,
     inputText,
     button,
@@ -10,6 +11,33 @@ import {
 import Game from './gameflow';
 
 const app = document.querySelector('#app');
+
+app.append(
+    wrapper('pre', asciiArt.submarine),
+    wrapper('pre', asciiArt.name),
+    wrapper('p', 'Presiona [Enter] o haz clic aquí para empezar'),
+    wrapper('pre', asciiArt.sea),
+);
+
+const startGame = (event) => {
+    if (event.type === 'pointerdown' || event.key === 'Enter') {
+        console.log('blablablablablablabla')
+        clearApp()
+
+        app.removeEventListener('pointerdown', startGame)
+        document.body.removeEventListener('keydown', startGame)
+
+        app.textContent = 'nnnn'
+    }
+
+
+
+
+
+}
+app.addEventListener('pointerdown', startGame)
+document.body.addEventListener('keydown', startGame)
+
 
 
 // const game = {
@@ -23,8 +51,6 @@ const app = document.querySelector('#app');
 //     },
 //
 //     landing: () => {
-//         header.innerHTML = `<pre>${asciiArt.submarine}</pre><pre>${asciiArt.name}</pre>`;
-//         app.innerHTML = `<p>Presiona cualquier tecla o haz clic para iniciar...</p><pre>${asciiArt.sea}</pre>`;
 //
 //         document.body.addEventListener('keydown', game.start);
 //         document.body.addEventListener('pointerdown', game.start);
